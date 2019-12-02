f282965c182ce06705972457238aed56782a5fb9
Jasinsk <jasinski.ja@gmail.com>
2019-12-02 16:51:02 +0100

    minor code refactor

--- SignalParser.py
+++ SignalParser.py
@@ -5,7 +5,7 @@ import librosa as lbr
 import librosa.display
 import math
 
-def PeakFinder(signal, samplingRate, threshold = -1, reachBackTime = 0.05, reachAheadTime = 0.1): #returns sample numbers of all offsets that exceed threshold
+def FindPeaks(signal, samplingRate, threshold = -1, reachBackTime = 0.05, reachAheadTime = 0.1): #returns sample numbers of all offsets that exceed threshold
     if threshold == -1:
         threshold = 0.6 * np.amax(signal)
     DetectedOffsets = librosa.onset.onset_detect(signal, samplingRate, backtrack=False, units="samples")
@@ -27,14 +27,14 @@ def PeakFinder(signal, samplingRate, threshold = -1, reachBackTime = 0.05, reach
 
     return parsingIndicator
 
-def RemoveDuplicatePeaks(peaks, samplingRate, minimalTimeDifference = 5):
+def RemoveDuplicatePeaks(peaks, samplingRate, minimalTimeDifference = 5): #trims peak list of all duplicates that are closer to each other then the minimalTimeDifference
     for i in range(0, len(peaks) - 1):
         if peaks[i] + samplingRate * minimalTimeDifference > peaks[i+1]:
             peaks[i+1] = 0
     peaks = np.ma.masked_equal(peaks,0).compressed()
     return peaks
 
-def ParseImpulses(signal, samplingRate, peaks, attackTime = 0.05, decayTime = 7):
+def ParseImpulses(signal, samplingRate, peaks, attackTime = 0.05, decayTime = 7): #returns array which has rows of impulses cut from the main signal around the found peaks
     impulses = []
     for el in peaks:
         impuls = signal[list(range(el - math.floor(attackTime * samplingRate), math.floor(el + decayTime * samplingRate)))]
@@ -44,7 +44,7 @@ def ParseImpulses(signal, samplingRate, peaks, attackTime = 0.05, decayTime = 7)
             impulses = np.vstack([impulses, impuls])
     return impulses
 
-def RemoveImpulsesWithEnergyDeviation(impulses, acceptableDeviation = 0.3):
+def RemoveImpulsesWithEnergyDeviation(impulses, acceptableDeviation = 0.3): #trims impulses that have too large of a energy deviation from the mean
     impulsEnergies = []
     for i in range (0, len(impulses[:,0])):
         impulsEnergies.append(sum((impulses[i,:]) * (impulses[i,:])))
@@ -53,33 +53,34 @@ def RemoveImpulsesWithEnergyDeviation(impulses, acceptableDeviation = 0.3):
     for el in impulsEnergies:
         if el > meanEnergy * (1 + acceptableDeviation) or el < meanEnergy * (1 - acceptableDeviation):
             impulses = np.delete(impulses, i, axis=0)
-            print("Energy Deviation!")
+            print("Energy Deviation Detected!")
         else:
             i += 1
     return impulses
 
-def WriteParsedImpulsesToFolder(impulses, sampleRate, folderName):
-    path = folderName
+def WriteParsedImpulsesToFolder(impulses, sampleRate, outputDirectory, seriesDirectory): #pushes impulses to files in a folder inside the output folder
+    path = outputDirectory + "/" + seriesDirectory
     os.mkdir(path)
     for i in range (0, len(impulses[:,0])):
-        filename = folderName + "/" + outputFolderName + "_" + str(i) + ".wav"
+        filename = path + "/" + seriesDirectory + "_" + str(i) + ".wav"
         librosa.output.write_wav(filename, impulses[i,:], sampleRate)
 
 # main
-inputDirectoryName = "SignalFolder"
-inputDirectory = os.fsencode(inputDirectoryName)
+inputDirectory = "InputFolder"
+outputDirectory = "OutputFolder"
+os.mkdir(outputDirectory)
 
-for seriesSignal in os.listdir(inputDirectory):
-    inputSignal = inputDirectoryName + "/" + os.fsdecode(seriesSignal)
-    outputFolderName = os.fsdecode(seriesSignal).rstrip('.wav')
+for seriesSignal in os.listdir(os.fsencode(inputDirectory)):
+    inputSignal = inputDirectory + "/" + os.fsdecode(seriesSignal)
+    seriesDirectory = os.fsdecode(seriesSignal).rstrip('.wav')
 
     signal, samplingRate = lbr.load(inputSignal)
 
-    foundPeaks = PeakFinder(signal, samplingRate)
+    foundPeaks = FindPeaks(signal, samplingRate)
     foundPeaks = RemoveDuplicatePeaks(foundPeaks, samplingRate)
     foundImpulses = ParseImpulses(signal, samplingRate, foundPeaks)
     validatedImpulses = RemoveImpulsesWithEnergyDeviation(foundImpulses, 0.17)
-    WriteParsedImpulsesToFolder(validatedImpulses, samplingRate, outputFolderName)
+    WriteParsedImpulsesToFolder(validatedImpulses, samplingRate, outputDirectory, seriesDirectory)
 
 #print(foundPeaks)
 
@@ -90,7 +91,7 @@ for seriesSignal in os.listdir(inputDirectory):
     plt.figure()
     librosa.display.waveplot(signal, samplingRate)
     librosa.display.waveplot(parsingIndicator, samplingRate)
-    plt.title(outputFolderName)
+    plt.title(seriesDirectory)
     plt.show()
 '''
     plt.figure()
