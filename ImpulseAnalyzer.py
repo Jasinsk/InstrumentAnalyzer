Fixed: HarmonicData calculation, Added: Spectral Irregularity Calculation

ImpulseAnalyzer.py
@@ -1,7 +1,5 @@
 import numpy as np
-from scipy import signal
 import os
-import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import iracema
@@ -20,9 +18,9 @@ def CalculateStatistics(values, meanValues, deviations):
     deviations.append(np.std(values))
     return 0
 
-def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fileNameAppendix, attackTime, sustainTime, \
-    centroid_flag, f0normCentroid_flag, rolloff_flag, bandwidth_flag, spread_flag, flux_flag, highLowEnergy_flag, \
-    subBandFlux_flag, tristimulus_flag, inharmonicity_flag, noisiness_flag, oddeven_flag, tuning_flag, crossingRate_flag, \
+def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fileNameAppendix, attackTime, sustainTime,
+    centroid_flag, f0normCentroid_flag, rolloff_flag, bandwidth_flag, spread_flag, flux_flag, irregularity_flag, highLowEnergy_flag,
+    subBandFlux_flag, tristimulus_flag, inharmonicity_flag, noisiness_flag, oddeven_flag, tuning_flag, crossingRate_flag,
     rms_flag, entropy_flag, temporalCentroid_flag, logAttackTime_flag, decayTime_flag, vectorOutput_flag):
 
     # clear output folder
@@ -35,7 +33,7 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
     data_array = []
     series_names, centroid_values, centroid_deviations,  f0NormalizedCentroid_values, f0NormalizedCentroid_deviations, \
     rolloff_values, rolloff_deviations, bandwidth_values, bandwidth_deviation, spread_values, spread_deviations, \
-    flux_values, flux_deviations, highLowEnergy_values, highLowEnegry_deviations, \
+    flux_values, flux_deviations, irregularity_values, irregularity_deviations, highLowEnergy_values, highLowEnegry_deviations, \
     subBandFlux1_values, subBandFlux1_deviations, subBandFlux2_values, subBandFlux2_deviations, \
     subBandFlux3_values, subBandFlux3_deviations, subBandFlux4_values, subBandFlux4_deviations, \
     subBandFlux5_values, subBandFlux5_deviations, subBandFlux6_values, subBandFlux6_deviations, \
@@ -50,7 +48,7 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
     decayTime_values, decayTime_deviations, foundFundumentalPitches = \
     [" "], ["Spectrum Centroid"], ["Centroid Deviation"], ["F0 Normalized Centroid"], ["F0 Normalized Centroid Deviations"],\
     ["Rolloff"], ["Rolloff Deviation"], ["Bandwidth"], ["Bandwidth Deviation"], ["Spread"], ["Spread Deviation"], \
-    ["Spectral Flux"], ["Spectral Flux Deviations"], ["High Energy - Low Energy Ratio"], ["High Energy - Low Energy Ratio Deviations"], \
+    ["Spectral Flux"], ["Spectral Flux Deviations"], ["Spectral Irregularity"], ["Spectral Irregularity Deviations"], ["High Energy - Low Energy Ratio"], ["High Energy - Low Energy Ratio Deviations"], \
     ["Sub-Band Flux 1"], ["Sub-Band Flux 1 Deviation"], ["Sub-Band Flux 2"], ["Sub-Band Flux 2 Deviation"], \
     ["Sub-Band Flux 3"], ["Sub-Band Flux 3 Deviation"], ["Sub-Band Flux 4"], ["Sub-Band Flux 4 Deviation"], \
     ["Sub-Band Flux 5"], ["Sub-Band Flux 5 Deviation"], ["Sub-Band Flux 6"], ["Sub-Band Flux 6 Deviation"], \
@@ -76,11 +74,11 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
         seriesDirectory = inputDirectory + "/" + os.fsdecode(seriesDirectory)
         print("Entering folder: " + seriesDirectory)
         impulses, attackSpectrums, sustainSpectrums, decaySpectrums, centroids, f0normCentroids, rolloffs, bandwidths, \
-        spreads, fluxes, highLowEnergies, subBandFluxes1, subBandFluxes2, subBandFluxes3, subBandFluxes4, subBandFluxes5, \
+        spreads, fluxes, irregularities, highLowEnergies, subBandFluxes1, subBandFluxes2, subBandFluxes3, subBandFluxes4, subBandFluxes5, \
         subBandFluxes6, subBandFluxes7, subBandFluxes8, subBandFluxes9, subBandFluxes10, \
         tristimulus1s, tristimulus2s, tristimulus3s, inharmonicities, noisinesses, \
         oddEvenRatios, tunings, crossingRates, rmss, entropies, temporalCentroids, logAttackTimes, decayTimes, \
-        pitchesHz =  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+        pitchesHz =  [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
 
         # If harmonic data and normalized centroids make no sense it may be caused by improper fundumental pitch detection.
         # Check in parameterData.csv whether the fundumentals were properly found.
@@ -118,7 +116,7 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
             if spread_flag:
                 spreads.append(np.mean(iracema.features.spectral_spread(args.impulseFFT).data))
             if flux_flag:
-                fluxes.append(np.mean(iracema.features.spectral_flux(args.impulseFFT).data))
+                fluxes.append(np.mean(iracema.features.spectral_flux(args.impulseFFT).data).real)
             if subBandFlux_flag:
                 subBandFlux = pc.CalculateSubBandSpectralFlux(args, samplingRate)
                 subBandFluxes1.append(subBandFlux[0])
@@ -165,6 +163,8 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
             noisinesses = pc.CalculateNoisiness(fullSpectrums, fullFrequencies, harmonicData)
         if highLowEnergy_flag:
             highLowEnergies = pc.CalculateHighEnergyLowEnergyRatio(fullSpectrums, fullFrequencies)
+        if irregularity_flag:
+            irregularities = pc.CalculateIrregularity(harmonicData)
         if tristimulus_flag:
             tristimulus1s, tristimulus2s, tristimulus3s = pc.CalculateTristimulus(harmonicData)
         if inharmonicity_flag:
@@ -198,6 +198,7 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
         CalculateStatistics(bandwidths, bandwidth_values, bandwidth_deviation)
         CalculateStatistics(spreads, spread_values, spread_deviations)
         CalculateStatistics(fluxes, flux_values, flux_deviations)
+        CalculateStatistics(irregularities, irregularity_values, irregularity_deviations)
         CalculateStatistics(highLowEnergies, highLowEnergy_values, highLowEnegry_deviations)
         CalculateStatistics(subBandFluxes1, subBandFlux1_values, subBandFlux1_deviations)
         CalculateStatistics(subBandFluxes2, subBandFlux2_values, subBandFlux2_deviations)
@@ -238,6 +239,8 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
         data_array = np.vstack((data_array, spread_values, spread_deviations))
     if flux_flag:
         data_array = np.vstack((data_array, flux_values, flux_deviations))
+    if irregularity_flag:
+        data_array = np.vstack((data_array, irregularity_values, irregularity_deviations))
     if highLowEnergy_flag:
         data_array = np.vstack((data_array, highLowEnergy_values, highLowEnegry_deviations))
     if subBandFlux_flag:
@@ -302,6 +305,9 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
         if flux_flag:
             dataWriter.writerow(flux_values)
             dataWriter.writerow(flux_deviations)
+        if irregularity_flag:
+            dataWriter.writerow(irregularity_values)
+            dataWriter.writerow(irregularity_deviations)
         if highLowEnergy_flag:
             dataWriter.writerow(highLowEnergy_values)
             dataWriter.writerow(highLowEnegry_deviations)
@@ -393,9 +399,9 @@ def run(inputDirectory, outputDirectory, parameterFileName, spectrumFileName, fi
     spectrum_array, temp_array = [], []
     for iterator in range(0, len(seriesNames)):
         outputFile = seriesNames[iterator].replace(inputDirectory, outputDirectory)
-        temp_array = [outputFile, allAttackSpectrums[iterator], \
-                                    allAttackFrequencies[iterator], allSustainSpectrums[iterator], \
-                                    allSustainFrequencies[iterator], allDecaySpectrums[iterator], \
+        temp_array = [outputFile, allAttackSpectrums[iterator],
+                                    allAttackFrequencies[iterator], allSustainSpectrums[iterator],
+                                    allSustainFrequencies[iterator], allDecaySpectrums[iterator],
                                     allDecayFrequencies[iterator]]
 
         spectrum_array.append(temp_array)

ParameterCalculator.py
@@ -85,7 +85,7 @@ def ExtractHarmonicDataFromSpectrums(spectrums, spectrumFrequencies, mathHarmoni
 
         for fftSample in range (0, len(spectrum)):
             if spectrumFrequencies[fftSample] > mathHarmonicFrequencies[harmonicNumber]:
-                peakValue, peakFrequency = 0, 0
+                peakValue, peakFrequency = -200, 0
 
                 for i in range (fftSample-buffor, fftSample+buffor):
                     if spectrum[i] >= peakValue:
@@ -134,6 +134,15 @@ def CalculateNoisiness(spectrums, frequencies, harmonicsData, harmonicWidth = 0.
 
         noisinesses.append((fullEnergy-harmonicEnergy)/fullEnergy)
     return noisinesses
+
+def CalculateIrregularity(harmonicData): # Calculates spectral harmonic irregularity according to Krimphoff, McAdams 1994
+    irregularities = []
+    for take in harmonicData:
+        irregularity = 0
+        for i in range (1, len(take.amplitudes)-1):
+            irregularity += abs(take.amplitudes[i] - np.mean(take.amplitudes[i - 1] + take.amplitudes[i] + take.amplitudes[i + 1]))
+        irregularities.append(np.log10(irregularity))
+    return irregularities
 
 def CalculateHighEnergyLowEnergyRatio(spectrums, frequencies, boundaryFrequency = 1500):
     highlowenergies = []