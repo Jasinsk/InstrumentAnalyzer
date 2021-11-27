7a8440b1fa7a436f7d294d520e062a25cdb05e09
Jasinsk <jasinski.ja@gmail.com>
2021-11-27 23:24:29 +0100

    Custom Graph creator upgrade - Isset version

--- CustomParameterDisplayer.py
+++ CustomParameterDisplayer.py
@@ -13,9 +13,9 @@ data = pd.read_csv("CustomParameterData.csv")
 vector = data.iloc[0, 1:].values.astype(np.float)
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-plt.errorbar(x, data.iloc[0, 1:].values.astype(np.float), data.iloc[1, 1:].values.astype(np.float), label="Shure SM57",fmt='rD', ecolor='r', elinewidth=1.5, capsize=15)
-plt.errorbar(x, data.iloc[2, 1:].values.astype(np.float), data.iloc[3, 1:].values.astype(np.float), label="Rode NT2", fmt='gD', ecolor='g', elinewidth=1.5, capsize=15)
-plt.errorbar(x, data.iloc[4, 1:].values.astype(np.float), data.iloc[5, 1:].values.astype(np.float), label="Piezo", fmt='bD', ecolor='b', elinewidth=1.5, capsize=15)
+plt.errorbar(x, data.iloc[0, 1:].values.astype(np.float), data.iloc[1, 1:].values.astype(np.float), label="Shure SM57", color='crimson', marker='D', linestyle='', ecolor='crimson', elinewidth=1.5, capsize=15)
+plt.errorbar(x, data.iloc[2, 1:].values.astype(np.float), data.iloc[3, 1:].values.astype(np.float), label="Rode NT2", color='limegreen', marker='D', linestyle='', ecolor='limegreen', elinewidth=1.5, capsize=15)
+plt.errorbar(x, data.iloc[4, 1:].values.astype(np.float), data.iloc[5, 1:].values.astype(np.float), label="Piezo", color='steelblue', marker='D', linestyle='', ecolor='steelblue', elinewidth=1.5, capsize=15)
 
 plt.xlabel('Głębokość [mm]', fontsize=customFontsize)
 plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ['25', '30', '35', '40', '45', '50', '55', '60', '65', '70'])
